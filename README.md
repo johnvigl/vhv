667bc8ae164e7a3a1064a8ca4950e2ddd5b33175
DimMilios <elles1992@yahoo.gr>
2022-03-10 17:09:26 +0200

    midi file export and docs

--- README.md
+++ README.md
@@ -34,3 +34,40 @@
 
 ### Αρχεία του Verovio Humdrum Viewer
 - `/src/js/vhv-scripts/*`
+
+### Ενσωμάτωση html-midi-player
+- Εγκατάσταση του `html-midi-player` μέσω `npm`
+  ```sh
+  npm install --save html-midi-player
+  ```
+  - Αν δε δουλεύει μπορεί να φορτωθεί απομακρυσμένα με την προσθήκη του παρακάτω script στο αρχείο `index.html`.
+    ```html
+    <script src="https://cdn.jsdelivr.net/combine/npm/tone@14.7.58,npm/@magenta/music@1.23.1/es6/core.js,npm/focus-visible@5,npm/html-midi-player@1.4.0"></script>
+    ``` 
+
+- Δημιουργία του script που ξεκινά το midi player: `src/js/html-midi.js`. Προσθέστε τα περιεχόμενα του αρχείου με το ίδιο όνομα που υπάρχει στο `github`.
+
+- Τα παρακάτω εισάγονται στο `index.html`
+  - Στο `<head>` element προστίθεται
+    ```html
+    <script type="module" src="/src/js/html-midi.js"></script>
+    ```
+  - Στο `<body>` element και μέσα στα elements που ακολουθούν (περίπου γραμμή 137)
+    ```html
+    <div class="topnav col-12 d-flex flex-column" id="topnav">
+        <div>
+            <div class="d-inline-flex w-100 ml-2 align-items-center"
+                style="white-space:nowrap; overflow: hidden;">
+    ```
+    Προσθέστε αυτά εδώ:
+    ```html
+      <button id="load-midi" class="btn btn-primary" type="button">Render file to MIDI</button>
+      <button id="save-midi" class="btn btn-primary" type="button">Export MIDI file</button>
+      <midi-player id="midi-player" sound-font></midi-player>
+    ```
+
+    - Αφαιρέστε τον παλιό player που βρίσκεται στο ίδιο σημείο
+      ```html
+        <div xmlns="http://www.w3.org/1999/xhtml" class="nav-play-button mr-5">
+      ```
+- Documentation [URL](https://github.com/cifkao/html-midi-player)
--- src/js/html-midi.js
+++ src/js/html-midi.js
@@ -2,25 +2,50 @@ import 'html-midi-player';
 import { getVrvWorker } from './humdrum-notation-plugin-worker.js';
 
 const player = document.getElementById('midi-player');
-const loadMidi = document.getElementById('load-midi');
+const loadMidiBtn = document.getElementById('load-midi');
+const saveMidiBtn = document.getElementById('save-midi');
 
 let vrvWorker = getVrvWorker();
 
-loadMidi.addEventListener('click', () => {
+// Convert Humdrum to MIDI and load MIDI to player
+loadMidiBtn.addEventListener('click', async () => {
   if (!vrvWorker) {
     console.error('Could not load Verovio Worker');
     return;
   }
-  
-  vrvWorker
-  .renderToMidi()
-  .then((base64midi) => {
+  player.src = await toMidi();  
+});
+
+// Convert Humdrum to MIDI and prompt file download
+saveMidiBtn.addEventListener('click', async () => {
+  if (!vrvWorker) {
+    console.error('Could not load Verovio Worker');
+    return;
+  }
+  downloadFile(await toMidi(), `song-${new Date().toISOString()}`);
+});
+
+// Convert Humdrum editor content to MIDI (base64 encoded)
+async function toMidi() {
+  try {
+    let base64midi = await vrvWorker.renderToMidi();
     let song = 'data:audio/midi;base64,' + base64midi;
     console.log('Midi song rendered by Verovio: ', song);
-    player.src = song;
-  })
-  .catch((err) => console.log('Error when trying to play midi', err));
-})
+    return song;
+  } catch(err) {
+    console.log('Error when trying to play midi', err);
+  }
+}
+
+function downloadFile(base64Song, fileName = '') {
+  let element = document.createElement('a');
+  element.setAttribute('href', base64Song);
+  element.setAttribute('download', fileName + '.mid');
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
 
 // For debugging
 window.loadMidiSong = () => {
